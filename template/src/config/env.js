a441c271241265d737796f70dc3f73ff97449d50
shubhamdev <shubhamsutar5799@gmail.com>
2025-12-06 21:48:57 +0530

    default port updated

--- template/src/config/env.js
+++ template/src/config/env.js
@@ -3,7 +3,7 @@ dotenv.config()
 
 export const config = {
   env: process.env.NODE_ENV || "development",
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 6000,
   mongoUri: process.env.MONGO_URI || "mongodb://localhost:27017/mydb",
   displayTimezone: process.env.DISPLAY_TIMEZONE || "UTC",
 }
