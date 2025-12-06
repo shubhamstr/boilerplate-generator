da2921adab80dd92b6176a4e4376b10c5b4a3075
shubhamdev <shubhamsutar5799@gmail.com>
2025-12-06 21:36:44 +0530

    error handling fixes

--- template/src/middleware/errorHandler.js
+++ template/src/middleware/errorHandler.js
@@ -1,5 +1,12 @@
 import { logger } from "./logger.js"
 
+export function notFoundHandler(req, res, next) {
+  res.status(404).json({
+    success: false,
+    message: "Route not found",
+  })
+}
+
 export function errorHandler(err, req, res, next) {
   logger.error({
     message: err.message,
