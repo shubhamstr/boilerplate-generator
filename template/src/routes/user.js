f84723a130103a761493bf671be70495dd84ff4a
shubhamdev <shubhamsutar5799@gmail.com>
2025-12-12 21:06:33 +0530

    removed template folder

--- template/src/routes/user.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express"
-import { userController } from "../controllers/user.controller.js"
-
-export const usersRouter = Router()
-
-usersRouter.get("/", userController.getUsers)
